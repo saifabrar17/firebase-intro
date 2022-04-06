6db1fc758037d96a766f842979ec7f0de3d53749
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-04-06 16:20:31 +0600

    signInWithPopUp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      console.log('error', error);
+    })
+}
+return (
+  <div className="App">
+    <button onClick={handleGoogleSignIn}>Google sign in</button>
+  </div>
+);
 }
 
 export default App;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAfng3r5lCz_XdfLynEccSco2S_PuBWDXs",
+  authDomain: "simple-firebase-authenti-f9152.firebaseapp.com",
+  projectId: "simple-firebase-authenti-f9152",
+  storageBucket: "simple-firebase-authenti-f9152.appspot.com",
+  messagingSenderId: "728153504131",
+  appId: "1:728153504131:web:4e1d37ce2255b635ab5eb6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
